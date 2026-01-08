80e2c15f99aa20d0ddfe82e1c5b7765095ef2847
Nahid Khan <nahidkhanniloy31@gmail.com>
2026-01-09 00:20:15 +0600

    aaded basic setup of MDB

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const cors = require('cors');
-const dotenv= require('dotenv').config();
+require('dotenv').config();
 
-// Load environmental variables from .env file
-dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// sample route
+app.get('/', (req, res)=>{
+    res.send('Parcel Server is running');
+})
+
+//Start the server 
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
